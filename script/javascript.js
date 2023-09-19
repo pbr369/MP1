ff596d3f61d7e059dbd5b93c86d0943ee1815067
pbr369 <143585864+pbr369@users.noreply.github.com>
2023-09-19 16:29:29 +0800

    updated

---
+++ script/javascript.js
@@ -0,0 +1,21 @@
+var myModal = document.getElementById("myModal");
+var myInput = document.getElementById("myInput");
+
+myModal.addEventListener("shown.bs.modal", function () {
+  myInput.focus();
+});
+
+$(document).ready(function () {
+  $("#show_hide_password a").on("click", function (event) {
+    event.preventDefault();
+    if ($("#show_hide_password input").attr("type") == "text") {
+      $("#show_hide_password input").attr("type", "password");
+      $("#show_hide_password i").addClass("fa-eye-slash");
+      $("#show_hide_password i").removeClass("fa-eye");
+    } else if ($("#show_hide_password input").attr("type") == "password") {
+      $("#show_hide_password input").attr("type", "text");
+      $("#show_hide_password i").removeClass("fa-eye-slash");
+      $("#show_hide_password i").addClass("fa-eye");
+    }
+  });
+});
